d4a4a528984510a605a638c2d7637375a68f5c0c
iamdanieljojokoomson <53906917+jojo-koomson@users.noreply.github.com>
2024-08-12 12:05:00 +0000

    commit 2

---
+++ src/collections/Media.ts
@@ -0,0 +1,25 @@
+import { CollectionConfig } from 'payload/types';
+
+const Media: CollectionConfig = {
+  slug: 'media',
+  upload: {
+    staticURL: '/media',
+    staticDir: 'media',
+    mimeTypes: ['image/*', 'video/*'],
+  },
+  fields: [
+    {
+      name: 'altText',
+      type: 'text',
+      label: 'Alt Text',
+      required: true,
+    },
+    {
+      name: 'caption',
+      type: 'text',
+      label: 'Caption',
+    },
+  ],
+};
+
+export default Media;
